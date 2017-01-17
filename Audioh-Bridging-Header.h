cad8231473f4dd698929a5fefd477f376f47c4a3
charles <charleshkang1@gmail.com>
2017-01-17 16:43:13 -0500

    added obj-c bridging header to implement spotify

---
+++ Audioh-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Audioh-Bridging-Header.h
+//  Audioh
+//
+//  Created by Charles Kang on 1/17/17.
+//  Copyright © 2017 Charles Kang. All rights reserved.
+//
+
+#ifndef Audioh_Bridging_Header_h
+#define Audioh_Bridging_Header_h
+
+
+#endif /* Audioh_Bridging_Header_h */
+
+#import <SpotifyAudioPlayback/SpotifyAudioPlayback.h>
+#import <SpotifyAuthentication/SpotifyAuthentication.h>
